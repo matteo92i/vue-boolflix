f4063f6725e961d4f32e6c7eb75f6d1e5a09ae52
Matteo92i <matteo.iovane@gmail.com>
2021-10-01 16:10:31 +0200

    inizio

---
+++ src/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: "Search",
+
+    data: function(){
+        return{
+
+        }
+    },
+
+    created: function (){
+        axios.get("https://api.themoviedb.org/3/movie/550?api_key=b44f67a7438dbc78b01da40ce05c127a")
+    }
+    
+
+    
+}
+</script>
+
+<style>
+
+</style>
